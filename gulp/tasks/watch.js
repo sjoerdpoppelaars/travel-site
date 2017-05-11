06d79417412e2b017ec2bbab5d5ec673c5999468
Sjoerd <sjoerd.poppelaars@gmail.com>
2017-05-11 20:31:32 +0200

    added gulp sprites tasks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,26 +1,27 @@
-var gulp            = require('gulp'),
-    watch           = require('gulp-watch'),
-    browserSync     = require('browser-sync').create();
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-// GULP WATCH TASK //
-gulp.task('watch', function(){
-    browserSync.init({
-        notify: false,
-        server: {
-            baseDir: "app"
-        }
-    })
-    watch('./app/index.html', function() {
-       browserSync.reload();
-    });
-    
-    watch('./app/assets/styles/**/*.css', function() {
-       gulp.start('cssInject'); 
-    });
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
 });
 
-// GULP cssInject TASK //
-gulp.task('cssInject', ['styles'], function(){
-   return gulp.src('./app/temp/styles/styles.css')
-   .pipe(browserSync.stream()); 
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
